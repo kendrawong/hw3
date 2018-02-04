eddb00e91412a5fe4b45c3d34e92308b834c14a2
kendrawong <kendra.m.wong@gmail.com>
2018-02-04 15:28:33 -0800

    Update horizontal-lines.js

--- horizontal-lines.js
+++ horizontal-lines.js
@@ -1,7 +1,8 @@
 //horizontal lines with a border of 10
+//spacing of 5
 
 var counter = 10
 while (counter <= width-10){
-  line(10, counter, width-10, counter)
+  line(10, counter, width-10, counter);
   counter = counter + 5
 }

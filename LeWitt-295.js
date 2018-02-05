8ac6468f371ca5d5f5255ae47aab5cad3c4ca433
kendrawong <kendra.m.wong@gmail.com>
2018-02-04 18:14:13 -0800

    Create LeWitt-295.js

---
+++ LeWitt-295.js
@@ -0,0 +1,14 @@
+//LeWitt 295
+//http://massmoca.org/event/walldrawing295/
+
+strokeWeight(3);
+noFill();
+ellipse((width/2), (width/2), width, height);
+rect(0, 0, width, height);
+rect(0, 0, 70, height);
+rect(0, 0, 340-70, height);
+line(70, 0, 0, 340); 
+line(340-70, 0, 340, 340);
+line(340, 0, 340-70, 340);
+line((width/2), 0, 0, 340);
+line((width/2), 0, 340, 340);

4a44e0dbdf5ecb0786f79b0370d9a2aa8e97a3e3
kendrawong <kendra.m.wong@gmail.com>
2018-02-04 17:36:07 -0800

    Create taller-lines.js

---
+++ taller-lines.js
@@ -0,0 +1,10 @@
+//space between lines 5
+//length added to lines 10
+
+var counter = 0;
+var length = 5;
+	while(counter <= width){
+    line(counter, 0, counter, length);
+    counter = counter + 5;
+    length = length + 10;
+  }

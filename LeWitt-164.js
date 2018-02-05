ec1ed82b3b683e4d32efccb8eeb9927039d87bf8
kendrawong <kendra.m.wong@gmail.com>
2018-02-04 18:59:47 -0800

    Create LeWitt-164.js

---
+++ LeWitt-164.js
@@ -0,0 +1,11 @@
+//LeWitt 164
+//http://massmoca.org/event/walldrawing164/
+
+createCanvas(340, 340);
+noFill();
+
+rect(20, 20, 300, 300);
+
+stroke('red');
+line(50, (height/2), 290, (height/2)); 
+line(100, 3*height/4, 240, height/4);
